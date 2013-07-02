064a872f447a4a960612c5a0e3593c3b98b039ab
Sebastian Kurfuerst <sebastian@typo3.org>
2013-07-02 12:17:12 +0200

    [FEATURE] add profiling for Neos

--- Classes/Sandstorm/Plumber/Package.php
+++ Classes/Sandstorm/Plumber/Package.php
@@ -39,8 +39,9 @@ class Package extends BasePackage {
 
 		$run = $profiler->start();
 		$dispatcher = $bootstrap->getSignalSlotDispatcher();
-		$run->setOption('Context', $bootstrap->getContext());
+		$run->setOption('Context', (string)$bootstrap->getContext());
 		$this->connectToSignals($dispatcher, $profiler, $run, $bootstrap);
+		$this->connectToNeosSignals($dispatcher, $profiler, $run, $bootstrap);
 	}
 
 	/**
@@ -92,6 +93,31 @@ class Package extends BasePackage {
 		});
 		$dispatcher->connect('TYPO3\Flow\Mvc\Dispatcher', 'afterControllerInvocation', function() use($run) {
 			$run->stopTimer('MVC: Controller Invocation');
+		});
+	}
+
+	/**
+	 * Wire signals to slots as needed in TYPO3 Neos.
+	 *
+	 * @param \TYPO3\Flow\SignalSlot\Dispatcher $dispatcher
+	 * @param \Sandstorm\PhpProfiler\Profiler $profiler
+	 * @param \Sandstorm\PhpProfiler\Domain\Model\ProfilingRun $run
+	 * @param \TYPO3\Flow\Core\Bootstrap $bootstrap
+	 * @return void
+	 */
+	protected function connectToNeosSignals(\TYPO3\Flow\SignalSlot\Dispatcher $dispatcher, \Sandstorm\PhpProfiler\Profiler $profiler, \Sandstorm\PhpProfiler\Domain\Model\ProfilingRun $run, \TYPO3\Flow\Core\Bootstrap $bootstrap) {
+		$dispatcher->connect('TYPO3\TypoScript\Core\Runtime', 'beginEvaluation', function($typoScriptPath) use($run) {
+			$run->startTimer('TypoScript Runtime: ' . $typoScriptPath);
+		});
+		$dispatcher->connect('TYPO3\TypoScript\Core\Runtime', 'endEvaluation', function($typoScriptPath) use($run) {
+			$run->stopTimer('TypoScript Runtime: ' . $typoScriptPath);
+		});
+
+		$dispatcher->connect('TYPO3\Neos\View\TypoScriptView', 'beginRender', function() use($run) {
+			$run->startTimer('Neos TypoScript Rendering');
+		});
+		$dispatcher->connect('TYPO3\Neos\View\TypoScriptView', 'endRender', function() use($run) {
+			$run->stopTimer('Neos TypoScript Rendering');
 		});
 	}
 
